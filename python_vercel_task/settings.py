f2df14a5af242124fdf948b1dbfee51b18737788
AlanDCE <escurraalandaniel@gmail.com>
2024-11-23 01:54:59 -0300

    dad
    dad

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY_env'
+SECRET_KEY = 'django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.App']
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
+WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
 
 
 # Database
@@ -78,12 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.pfgjfyrbjydysuleleyq',
-        'PASSWORD': config('password_supabase_env'),
+        'USER': 'postgres.zluonypofjsulnxvexze',
+        'PASSWORD': 'vxHOyCcFX8hWD9W6',  # Reemplaza con la contraseña correcta.
         'HOST': 'aws-0-us-west-1.pooler.supabase.com',
         'PORT': '6543',
     }
 }
+
 
 
 # Password validation
